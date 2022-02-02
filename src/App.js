50ffd2e0cf4708b1503f9e5785ddff12c1a37ab2
Asya Bakhnar <abakhnar@oggettoweb.com>
2022-02-02 23:08:38 +0300

    Add Cart context and CartProvider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [isShowCart, setIsShowCart] = useState(false);
@@ -16,13 +17,13 @@ function App() {
   }
 
   return (
-      <>
+      <CartProvider>
         { isShowCart && <Cart onClose={ hideCartHandler }/> }
         <Header onShowCart={ showCartHandler }/>
         <main>
           <Meals/>
         </main>
-      </>
+      </CartProvider>
   );
 }
 
